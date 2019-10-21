cc3c4b18dc7d2e91f08c4e4e12fda242fc7abe4a
kyriosdata <fabio@inf.ufg.br>
2019-10-21 14:22:32 -0300

    padrao

--- sandbox/src/main/java/com/github/kyriosdata/healthcodec/Buffer.java
+++ sandbox/src/main/java/com/github/kyriosdata/healthcodec/Buffer.java
@@ -1,16 +1,16 @@
 /*
-* Copyright 2019 Instituto de Informática - UFG
-*
-* Licensed under the Apache License, Version 2.0 (the "License");
-* you may not use this file except in compliance with the License.
-* You may obtain a copy of the License at
-*    http://www.apache.org/licenses/LICENSE-2.0
-* 
-* Unless required by applicable law or agreed to in writing, software
-* distributed under the License is distributed on an "AS IS" BASIS,
-* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-* See the License for the specific language governing permissions and
-* limitations under the License.
+ * Copyright 2019 Instituto de Informática - UFG
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 package com.github.kyriosdata.healthcodec;
 
@@ -21,19 +21,21 @@ import java.nio.ByteBuffer;
 import java.nio.charset.StandardCharsets;
 
 /**
+ * Classe Responsável por primeiramente encapsular o
+ * * buffer de dados e fornecer as operações de E/S sobre o mesmo.
  *
- * @author Gabriel Descrição: Classe Responsável por primeiramente encapsular o
- * buffer de dados e fornecer as operações de E/S sobre o mesmo
+ * @author Gabriel
  */
 public class Buffer {
 
-    //Tamanho máximo assumido pelo buffer que armazenará os dados serializados
+    /**
+     * Tamanho máximo do buffer.
+     */
     static final int MAX_SIZE_BUFFER = 1024;
 
     /**
      * Buffer que será utilizado para armazenar os headers e os dados passados
      * como argumento na função serialize. Veja {@link #serialize(byte[])}
-     *
      */
     private ByteBuffer buffer;
 
@@ -42,17 +44,17 @@ public class Buffer {
 
     }
 
-    /*
-    * Função responsável por encapsular um array de bytes 
-    * recebido com entrada em um ByteBuffer que será armazenado
-    * no atributo da classe
-    *
-    * @param data array de bytes que é recebido como entrada
-    *             contendo os headers (metadados e informações importantes
-    *             como o tamanho de uma string, por exemplo) e os conteúdos
-    *             associado a cada header presente no array
-    * @return Retorna uma instância com o buffer encapsulado e pronto para
-    *         as operações de E/S
+    /**
+     * Função responsável por encapsular um array de bytes
+     * recebido com entrada em um ByteBuffer que será armazenado
+     * no atributo da classe
+     *
+     * @param data array de bytes que é recebido como entrada
+     *             contendo os headers (metadados e informações importantes
+     *             como o tamanho de uma string, por exemplo) e os conteúdos
+     *             associado a cada header presente no array
+     * @return Retorna uma instância com o buffer encapsulado e pronto para
+     * as operações de E/S
      */
     public static Buffer serialize() {
 
@@ -63,17 +65,17 @@ public class Buffer {
         return bf;
     }
 
-    /*
-    * Função responsável por encapsular um array de bytes 
-    * recebido com entrada em um ByteBuffer que será armazenado
-    * no atributo da classe
-    *
-    * @param data array de bytes que é recebido como entrada
-    *             contendo os headers (metadados e informações importantes
-    *             como o tamanho de uma string, por exemplo) e os conteúdos
-    *             associado a cada header presente no array
-    * @return Retorna uma instância com o buffer encapsulado e pronto para
-    *         as operações de E/S
+    /**
+     * Função responsável por encapsular um array de bytes
+     * recebido com entrada em um ByteBuffer que será armazenado
+     * no atributo da classe
+     *
+     * @param data array de bytes que é recebido como entrada
+     *             contendo os headers (metadados e informações importantes
+     *             como o tamanho de uma string, por exemplo) e os conteúdos
+     *             associado a cada header presente no array
+     * @return Retorna uma instância com o buffer encapsulado e pronto para
+     * as operações de E/S
      */
     public static Buffer deserialize(Buffer b) {
         Buffer bf = new Buffer();
@@ -82,15 +84,15 @@ public class Buffer {
         return bf;
     }
 
-    /*
-    * Função responsável por ler 4 bytes do buffer e retornar o número
-    * inteiro que corresponde a sequência de bytes lida.
-    *
-    * @param position Posição do array
-    * @return intValue Valor inteiro correspondente aos 4 btyes lidos na 
-    * operação
-    * @throws BufferUnderflowException no caso de não conseguir ler 4 bytes do 
-    * buffer
+    /**
+     * Função responsável por ler 4 bytes do buffer e retornar o número
+     * inteiro que corresponde a sequência de bytes lida.
+     *
+     * @param position Posição do array
+     * @return intValue Valor inteiro correspondente aos 4 btyes lidos na
+     * operação
+     * @throws BufferUnderflowException no caso de não conseguir ler 4 bytes do
+     *                                  buffer
      */
     public int readInteger(int position) throws BufferUnderflowException {
         int intValue;
@@ -98,27 +100,28 @@ public class Buffer {
         return intValue;
     }
 
-    /*
-    * Função responsável por escrever um número inteiro (4 bytes) no buffer
-    *
-    * @param position Posição do array
-    * @param i Número inteiro que será escrito
-    * @throws IndexOutOfBoundsException no caso de uma posição maior que o 
-    * tamanho do array
-    * @throws ReadOnlyBufferException no caso do buffer ser apenas para escrita
+    /**
+     * Função responsável por escrever um número inteiro (4 bytes) no buffer
+     *
+     * @param position Posição do array
+     * @param i        Número inteiro que será escrito
+     * @throws IndexOutOfBoundsException no caso de uma posição maior que o
+     *                                   tamanho do array
+     * @throws ReadOnlyBufferException   no caso do buffer ser apenas para
+     * escrita
      */
-    public void writeInteger(int position, int i){
+    public void writeInteger(int position, int i) {
         buffer.position(position);
         buffer.putInt(position, i);
     }
 
-    /*
-    * Função responsável por ler 1 byte do buffer e retorná-lo
-    *
-    * @param position Posição do array
-    * @return byteValue correspondente
-    * @throws BufferUnderflowException no caso de não conseguir ler 4 bytes 
-    * do buffer
+    /**
+     * Função responsável por ler 1 byte do buffer e retorná-lo
+     *
+     * @param position Posição do array
+     * @return byteValue correspondente
+     * @throws BufferUnderflowException no caso de não conseguir ler 4 bytes
+     *                                  do buffer
      */
     public byte readByte(int position) throws BufferUnderflowException {
         byte byteValue;
@@ -127,35 +130,36 @@ public class Buffer {
     }
 
     /*
-    * Função responsável por escrever um único byte no buffer
-    *
-    * @param position Posição do array
-    * @param b byte que será escrito
-    * @throws IndexOutOfBoundsException no caso de uma posição maior que o 
-    * tamanho do array
-    * @throws ReadOnlyBufferException no caso do buffer ser apenas para escrita
+     * Função responsável por escrever um único byte no buffer
+     *
+     * @param position Posição do array
+     * @param b byte que será escrito
+     * @throws IndexOutOfBoundsException no caso de uma posição maior que o
+     * tamanho do array
+     * @throws ReadOnlyBufferException no caso do buffer ser apenas para escrita
      */
     public void writeByte(int position, byte b) {
         buffer.position(position);
         buffer.put(position, b);
     }
 
-    /*
-    * Função responsável por ler um array de bytes do buffer a partir de 
-    * uma posição inicial e da quantidade de bytes que serão lidos a partir
-    * desta posição
-    *
-    * @param position Posição do array
-    * @param length Quantidade de bytes que serão lidos a partir da posição 
-    * incial
-    * @return byteArray Retorna o array de bytes que armazena os bytes 
-    * solicitados.
-    * @throws BufferUnderflowException no caso de não conseguir ler a quantidade
-    * de bytes do buffer
-    * @throws IndexOutOfBoundsException no caso de não conseguir acessar a 
-    * posição no buffer
+    /**
+     * Função responsável por ler um array de bytes do buffer a partir de
+     * uma posição inicial e da quantidade de bytes que serão lidos a partir
+     * desta posição
+     *
+     * @param position Posição do array
+     * @param length   Quantidade de bytes que serão lidos a partir da posição
+     *                 incial
+     * @return byteArray Retorna o array de bytes que armazena os bytes
+     * solicitados.
+     * @throws BufferUnderflowException  no caso de não conseguir ler a
+     *                                   quantidade
+     *                                   de bytes do buffer
+     * @throws IndexOutOfBoundsException no caso de não conseguir acessar a
+     *                                   posição no buffer
      */
-    public byte[] readByteArray(int position, int length){
+    public byte[] readByteArray(int position, int length) {
         byte[] byteArray = new byte[length];
         int j = 0;
         for (int i = position; i < (position + length); i++) {
@@ -165,21 +169,22 @@ public class Buffer {
         return byteArray;
     }
 
-    /*
-    * Função responsável por escrever um array de bytes do buffer a partir de 
-    * uma posição inicial e da quantidade de bytes que serão lidos a partir
-    * desta posição
-    *
-    * @param position Posição do array
-    * @param b[] Array de bytes fonte que será armazenado no buffer
-    * @throws BufferUnderflowException no caso de não conseguir ler a quantidade
-    * de bytes do buffer
-    * @throws IndexOutOfBoundsException no caso de não conseguir acessar a 
-    * posição no buffer
-    * @throws BufferOverflowException se não existe espaço suficiente para 
-    * armazenar o array de bytes
+    /**
+     * Função responsável por escrever um array de bytes do buffer a partir de
+     * uma posição inicial e da quantidade de bytes que serão lidos a partir
+     * desta posição
+     *
+     * @param position Posição do array
+     * @param b[]      Array de bytes fonte que será armazenado no buffer
+     * @throws BufferUnderflowException  no caso de não conseguir ler a
+     *                                   quantidade
+     *                                   de bytes do buffer
+     * @throws IndexOutOfBoundsException no caso de não conseguir acessar a
+     *                                   posição no buffer
+     * @throws BufferOverflowException   se não existe espaço suficiente para
+     *                                   armazenar o array de bytes
      */
-    public void writeByteArray(int position, byte[] b){
+    public void writeByteArray(int position, byte[] b) {
         int j = 0;
         for (int i = position; i < (position + b.length); i++) {
             writeByte(i, b[j]);
@@ -187,19 +192,22 @@ public class Buffer {
         }
     }
 
-    /*
-    * Função responsável por ler um array de bytes do buffer a partir de 
-    * uma posição inicial e da quantidade de bytes que serão lidos a partir
-    * desta posição. Esse array de bytes é então convertido em uma String
-    * após a conversão de cada caractere para UTF-8
-    *
-    * @param position Posição do array
-    * @param length Quantidade de bytes que serão lidos a partir da posição incial
-    * @return string Retorna a String resultante da leitura dos bytes
-    * @throws BufferUnderflowException no caso de não conseguir ler a quantidade de bytes do buffer
-    * @throws IndexOutOfBoundsException no caso de não conseguir acessar a posição no buffer
+    /**
+     * Função responsável por ler um array de bytes do buffer a partir de
+     * uma posição inicial e da quantidade de bytes que serão lidos a partir
+     * desta posição. Esse array de bytes é então convertido em uma String
+     * após a conversão de cada caractere para UTF-8
+     *
+     * @param position Posição do array
+     * @param length   Quantidade de bytes que serão lidos a partir da posição
+     *                 incial
+     * @return string Retorna a String resultante da leitura dos bytes
+     * @throws BufferUnderflowException  no caso de não conseguir ler a
+     *                                   quantidade de bytes do buffer
+     * @throws IndexOutOfBoundsException no caso de não conseguir acessar a
+     *                                   posição no buffer
      */
-    public String readString(int position, int length){
+    public String readString(int position, int length) {
         byte[] byteArray = new byte[length];
         int j = 0;
         for (int i = position; i < (position + length); i++) {
@@ -209,18 +217,18 @@ public class Buffer {
         return getStringFromByteArray(byteArray);
     }
 
-    /*
-    * Função responsável por escrever uma String no buffer a partir de 
-    * uma posição inicial e da quantidade de bytes que serão lidos a partir
-    * desta posição. Cada caracter dessa String será primeiramente convertido
-    * em UTF-8.
-    *
-    * @param position Posição do array
-    * @param text String que será codificada e armazenada
-    * @throws IndexOutOfBoundsException no caso de não conseguir acessar a 
-    * posição no buffer
-    * @throws BufferOverflowException se não existe espaço suficiente para 
-    * armazenar o array de bytes
+    /**
+     * Função responsável por escrever uma String no buffer a partir de
+     * uma posição inicial e da quantidade de bytes que serão lidos a partir
+     * desta posição. Cada caracter dessa String será primeiramente convertido
+     * em UTF-8.
+     *
+     * @param position Posição do array
+     * @param text     String que será codificada e armazenada
+     * @throws IndexOutOfBoundsException no caso de não conseguir acessar a
+     *                                   posição no buffer
+     * @throws BufferOverflowException   se não existe espaço suficiente para
+     *                                   armazenar o array de bytes
      */
     public void writeString(int position, String text) {
         byte[] b = getUTF8BytesFromString(text);
@@ -231,14 +239,14 @@ public class Buffer {
         }
     }
 
-    /*
-    * Função responsável por ler 1 byte do buffer e retornar o byte 
-    * correspondente ao valor lógico armazenado
-    *
-    * @param position Posição do array
-    * @return booleanValue correspondente
-    * @throws BufferUnderflowException no caso de não conseguir ler 4 bytes do 
-    * buffer
+    /**
+     * Função responsável por ler 1 byte do buffer e retornar o byte
+     * correspondente ao valor lógico armazenado
+     *
+     * @param position Posição do array
+     * @return booleanValue correspondente
+     * @throws BufferUnderflowException no caso de não conseguir ler 4 bytes do
+     *                                  buffer
      */
     public boolean readBoolean(int position) throws BufferUnderflowException {
         boolean booleanValue;
@@ -248,14 +256,15 @@ public class Buffer {
         return booleanValue;
     }
 
-    /*
-    * Função responsável por escrever um único valor booleano no buffer
-    *
-    * @param position Posição do array
-    * @param b boolean que será escrito
-    * @throws IndexOutOfBoundsException no caso de uma posição maior que o 
-    * tamanho do array
-    * @throws ReadOnlyBufferException no caso do buffer ser apenas para escrita
+    /**
+     * Função responsável por escrever um único valor booleano no buffer
+     *
+     * @param position Posição do array
+     * @param b        boolean que será escrito
+     * @throws IndexOutOfBoundsException no caso de uma posição maior que o
+     *                                   tamanho do array
+     * @throws ReadOnlyBufferException   no caso do buffer ser apenas para
+     * escrita
      */
     public void writeBoolean(int position, boolean b)
             throws IndexOutOfBoundsException,
@@ -263,37 +272,40 @@ public class Buffer {
         buffer.position(position);
         buffer.put(position, (byte) (b ? 1 : 0));
     }
-    
-    /*
-    * Função responsável por obter os btyes UTF-8 de uma String
-    * @param s String de entrada
-    * @return array de bytes contendo a codificação
-    */
+
+    /**
+     * Função responsável por obter os btyes UTF-8 de uma String
+     *
+     * @param s String de entrada
+     * @return array de bytes contendo a codificação
+     */
     public byte[] getUTF8BytesFromString(String s) {
         byte[] bytes = new byte[s.length()];
         try {
             bytes = s.getBytes("UTF-8");
-        } catch(UnsupportedEncodingException e){
+        } catch (UnsupportedEncodingException e) {
             //TODO
         }
         return bytes;
     }
-    
-    /*
-    * Função repsonsável por receber um array de bytes com os bytes na codificação
-    * UTF-8 e realizar a conversão da mesma em String
-    * 
-    * @params utf8ByteArray Array de bytes com os bytes da String em UTF-8
-    * @return a String convertida
-    */
+
+    /**
+     * Função repsonsável por receber um array de bytes com os bytes na
+     * codificação
+     * UTF-8 e realizar a conversão da mesma em String
+     *
+     * @return a String convertida
+     * @params utf8ByteArray Array de bytes com os bytes da String em UTF-8
+     */
     public String getStringFromByteArray(byte[] utf8ByteArray) {
         return new String(utf8ByteArray, StandardCharsets.UTF_8);
     }
 
-    /*
-    *   Função responsável por retornar a representação em array e bytes
-    *   do buffer (ByteBuffer)
-    *   @return byte[] data Array de bytes correspondente ao buffer
+    /**
+     * Função responsável por retornar a representação em array e bytes
+     * do buffer (ByteBuffer)
+     *
+     * @return byte[] data Array de bytes correspondente ao buffer
      */
     public byte[] data() {
         return buffer.array();
