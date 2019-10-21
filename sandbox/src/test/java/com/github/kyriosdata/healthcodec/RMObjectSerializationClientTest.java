9be120868386f39d5ce3c46aa9bdb506b4c72075
Gabriel Soares <gabrielsxp@hotmail.com>
2019-10-21 17:48:02 -0300

    Adicionada nova classe de teste

---
+++ sandbox/src/test/java/com/github/kyriosdata/healthcodec/RMObjectSerializationClientTest.java
@@ -0,0 +1,16 @@
+package com.github.kyriosdata.healthcodec;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+
+class RMObjectSerializationClientTest {
+
+    @BeforeEach
+    void setUp() {
+
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+}
